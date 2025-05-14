e03668aee4d8f948f2640bd43ef075f04af6b53b
Muddasir <muddasirmd2@gmail.com>
2025-05-14 23:44:29 +0500

    Create Folder Backend Functionality

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreFolderRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\File;
+use Illuminate\Support\Facades\Auth;
 
 class FileController extends Controller
 {
@@ -12,8 +15,24 @@ class FileController extends Controller
         return Inertia::render('MyFiles');
     }
 
-    public function createFolder()
+    public function createFolder(StoreFolderRequest $request)
     {
-        
+        $data = $request->validated();
+        $parent = $request->parent;
+
+        if(!$parent){
+            $parent = $this->getRoot();
+        }
+
+        $file = new File();
+        $file->is_folder = 1;
+        $file->name = $data['name'];
+
+        $parent->appendNode($file);
+    }
+
+    private function getRoot()
+    {
+      return File::query()->whereIsRoot()->where('created_by', Auth::id())->firstOrFail();
     }
 }
