e2431cb90adb1d9a89af55283f3d3595635360dd
Muddasir <muddasirmd2@gmail.com>
2025-06-24 00:11:01 +0500

    Breadcrum Styling

--- resources/js/Pages/MyFiles.vue
+++ resources/js/Pages/MyFiles.vue
@@ -1,6 +1,26 @@
 <template>
     <AuthenticatedLayout>
-        {{ ancestors }}
+       <nav class="flex items-center justify-between p-1 mb-3">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+            <li v-for="ans of ancestors.data" :key="ans.id" class="inline-flex item-center">
+                <Link v-if="!ans.parent_id" :href="route('myFiles')" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                    <HomeIcon class="w-4 h-4 mr-2" />
+                    My Files
+                </Link>
+                <div v-else class="flex items-center">
+                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                    </svg>
+                    <Link :href="route('myFiles', {folder: ans.path})" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 
+                    md:ml-2 dark:text-gray-400 dark:hover:text-white">
+                        {{ ans.name }}
+                    </Link>
+                </div>
+            </li>
+
+        </ol>
+
+       </nav>
         <table class="min-w-full">
             <thead class="bg-gray-100 border-b">
                 <tr>
@@ -45,7 +65,8 @@
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { router } from '@inertiajs/vue3'
+import { router, Link } from '@inertiajs/vue3'
+import { HomeIcon } from '@heroicons/vue/20/solid'
 
 const {files} = defineProps({
     files: Object,
